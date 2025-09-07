cfb3ce14d21a87b1444cc1c945cf9c9eef0ae884
Afua Hamissi <lyhamissi@example.com>
2025-09-07 18:51:28 +0200

    resloved conflicts

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,45 +4,25 @@ import Home from './Components/Home'
 import Layout from './Components/Layout'
 import PageDetails from './Components/ProductDetails'; 
 import './App.css'
-import Sidebar from './AdminDashboard/Sidebar'
-import DashboardNavbar from './AdminDashboard/Navbar'
->>>>>>>>> Temporary merge branch 2
+import Sidebar from './AdminDashboard/Sidebar';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <BrowserRouter>
-<<<<<<<<< Temporary merge branch 1
-        <Routes>
-          {/* Layout wrapper */}
-          <Route path='/' element={<Layout />}>
-            
-            {/* Home page */}
-            <Route index element={<Home />} />
+       <BrowserRouter>
+  
+   <Routes>
 
-            {/* ✅ Page details route */}
-            <Route path='/product/:id' element={<PageDetails />} />
-
-          </Route>
-        </Routes>
-=========
-
-        <Routes>
-
-          <Route path='/' element={<Layout />}>
-            <Route path='/Home' index element={<Home />} />
-
-          </Route>
-          <Route path='/Dashboard' index element={<Sidebar />} />
-          <Route path='/Dashboard' index element={<DashboardNavbar />} />
-
-
-        </Routes>
-
->>>>>>>>> Temporary merge branch 2
-      </BrowserRouter>
+     <Route path='/' element={<Layout/>}> 
+     <Route path='/Home'  index element={<Home/>}/>
+      <Route path='/Product/:id' element={<PageDetails/>}/>
+     </Route>
+     <Route path='dashboard' element={<Sidebar/>}/>
+   </Routes>
+  
+  </BrowserRouter>
     </>
   );
 }
